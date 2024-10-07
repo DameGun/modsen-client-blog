f33449aca22eefb71bd6e688793f0fcb5fa1da54
Ilya Prymak <primak2506@gmail.com>
2024-10-07 12:23:28 +0300

    fix: cors policy path

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   headers: async () => {
     return [
       {
-        source: '/:locale/contact',
+        source: '/contact',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
